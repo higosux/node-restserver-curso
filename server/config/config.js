e3457dff8483d881e535064396fa677a39f6e985
Gonzalo <gonzalaos@gmail.com>
2020-07-19 17:00:24 -0400

    Variable d eentorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/Cafe';
 } else {
-    urlDB = 'mongodb+srv://strer:sKj72A5992GBkoU2@cluster0.gp4at.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
 //mongodb://localhost:27017/Cafe
